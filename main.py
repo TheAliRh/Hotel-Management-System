33b96d4da76781d275d8bc2a8f277973f7b31129
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-05-04 17:28:48 +0330

    Creating initial POST GET PUT DELETE methods

---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+
+# making an instance of the FastAPI
+app = FastAPI()
+
+# Test data
+customers = {
+    1: {
+        "firstname": "john",
+        "lastname": "doe",
+        "id": "059 999 99 99",
+        "nationality": "Iran",
+        "status": "present",
+    }
+}
+
+rooms = {1: {"room_number": 1, "type": "master", "status": "empty"}}
+
+# POST methods
+
+
+@app.post("/api/login")
+def login(username: str, password: str):
+    pass
+
+
+@app.post("/api/new-customer")
+def post_customer():
+    return {"Message": "customer successfully added!"}
+
+
+# GET methods
+
+
+@app.get("/api/customers")
+def get_customers():
+    return customers
+
+
+@app.get("/api/rooms")
+def get_rooms():
+    return rooms
+
+
+# PUT methods
+
+
+@app.put("/api/room/{room_number}")
+def update_room(room_number: int, status: str):
+    rooms[room_number]["status"] = status
+
+
+# Delete methods
