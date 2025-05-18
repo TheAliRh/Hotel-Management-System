04ff2eb05f67c2c904070ecc186d40ebfef8a065
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-05-18 15:15:45 +0330

    Filling the room and customer dal files with necessary classes and methods.

---
+++ backend/src/dal/customer.py
@@ -0,0 +1,122 @@
+"""
+Isolated data access layer of customers.
+"""
+
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+
+class Customer(BaseModel):
+    firstname: str = None
+    lastname: str = None
+    id: str = None
+    phone: str = None
+    nationality: str = None
+    status: bool = None  # Active=true, Inactive=False
+    room: int = None
+
+    @staticmethod
+    def from_doc(doc):
+        return Customer(
+            firstname=str(doc["firstname"]),
+            lastname=str(doc["lastname"]),
+            id=str(doc["id"]),
+            phone=str(doc["phone"]),
+            nationality=str(doc["nationality"]),
+            status=bool(doc["status"]),
+            room=int(doc["room"]),
+        )
+
+
+class CustomerDAL:
+    def __init__(self, customer_collection: AsyncIOMotorCollection):
+
+        self._customer_collection = customer_collection
+
+    """
+    Create new customer
+    """
+
+    async def create_customer(
+        self,
+        firstname: str = None,
+        lastname: str = None,
+        id: str = None,
+        phone: str = None,
+        nationality: str = None,
+        status: bool = None,
+        room: int = None,
+        session=None,
+    ) -> str:
+        response = await self._customer_collection.insert_one(
+            {
+                "firstname": firstname,
+                "lastname": lastname,
+                "id": id,
+                "phone": phone,
+                "nationality": nationality,
+                "status": status,
+                "room": room,
+            },
+            session=session,
+        )
+        return str(response.inserted_id)
+
+    """
+    List customers
+    """
+
+    async def list_customers(self, session=None) -> Customer:
+        doc = await self._customer_collection.find({}, {}, session=session)
+        return Customer.from_doc(doc)
+
+    """
+    Show customer
+    """
+
+    async def get_customer(
+        self,
+        id: str = None,
+        session=None,
+    ) -> Customer:
+        doc = await self._customer_collection.find_one(
+            {
+                "id": id,
+            },
+            session=session,
+        )
+        return Customer.from_doc(doc)
+
+    """
+    Update customer
+    """
+
+    async def update_customer(
+        self,
+        id: str = None,
+        session=None,
+    ) -> Customer:
+        doc = await self._customer_collection.find_one(
+            {
+                "id": id,
+            },
+            session=session,
+        )
+        return Customer.from_doc(doc)
+
+    """
+    Delete customer
+    """
+
+    async def delete_customer(
+        self,
+        id: str = None,
+        session=None,
+    ) -> bool:
+        response = await self._customer_collection.delete_one(
+            {
+                "id": id,
+            },
+            session=session,
+        )
+        return response.deleted_count == 1
---
+++ backend/src/dal/room.py
@@ -0,0 +1,69 @@
+"""
+Isolated data access layer of rooms.
+"""
+
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+
+class Room(BaseModel):
+    number: int = None
+    type: str = None
+    status: bool = None  # Empty=True, Full=False
+
+    @staticmethod
+    def from_doc(doc):
+        return Room(
+            number=int(doc["number"]), type=str(doc["type"]), status=bool(doc["status"])
+        )
+
+
+class RoomDAL:
+    def __init__(self, room_collection: AsyncIOMotorCollection):
+        self._room_collection = room_collection
+
+    """
+    Create room
+    """
+
+    async def create_room(
+        self, number: int = None, type: str = None, status: str = None, session=None
+    ) -> str:
+        response = await self._room_collection.insert_one(
+            {"number": number, "type": type, "status": status}, session=session
+        )
+        return str(response.inserted_id)
+
+    """
+    List all rooms
+    """
+
+    async def list_rooms(self, session=None):
+        doc = await self._room_collection.find({}, {}, session=session)
+        return Room.from_doc(doc)
+
+    """
+    Show room
+    """
+
+    async def get_room(self, number: int = None, session=None) -> Room:
+        doc = await self._room_collection.find_one({"number": number}, session=session)
+        return Room.from_doc(doc)
+
+    """
+    Update room
+    """
+
+    async def update_room(self, number: int = None, session=None) -> Room:
+        doc = await self._room_collection.find_one({"number": number}, session=session)
+        return Room.from_doc(doc)
+
+    """
+    Delete room
+    """
+
+    async def delete_room(self, number: int = None, session=None) -> bool:
+        response = await self._room_collection.delete_one(
+            {"number": number}, session=session
+        )
+        return response.deleted_count == 1
